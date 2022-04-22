710b34a0d8ed41a9f5acbd7b6755f4024daa64b8
Darryl <dar.goh.96@gmail.com>
2022-04-22 17:47:49 +0800

    Error and result handling

--- src/components/RevokeAllowance.js
+++ src/components/RevokeAllowance.js
@@ -1,60 +1,70 @@
 import { Fee, MsgExecuteContract, MsgSend } from '@terra-money/terra.js';
 import {
-  CreateTxFailed,
-  Timeout,
-  TxFailed,
-  TxUnspecifiedError,
-  useConnectedWallet,
-  UserDenied,
+    CreateTxFailed,
+    Timeout,
+    TxFailed,
+    TxUnspecifiedError,
+    useConnectedWallet,
+    UserDenied,
 } from '@terra-money/wallet-provider';
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 export function useRevokeAllowance() {
-  const connectedWallet = useConnectedWallet();
+    const [txResult, setTxResult] = useState(null);
+    const [txError, setTxError] = useState(null);
 
-  const revokeAllowance = (tokenAddress, allowance, spender) => {
-    if (!connectedWallet) {
-      return;
-    }
-  
-    connectedWallet
-      .post({
-        fee: new Fee(1000000, '200000uusd'),
-        msgs: [
-          new MsgExecuteContract(
-            connectedWallet.walletAddress,
-            tokenAddress,
-            {
-              "decrease_allowance": {
-                "spender": spender,
-                "amount": allowance,
-                "expires": {
-                  "at_height": 1
+    const connectedWallet = useConnectedWallet();
+
+    const revokeAllowance = (tokenAddress, allowance, spender) => {
+        if (!connectedWallet) {
+            return;
+        }
+
+        setTxResult(null);
+        setTxError(null);
+
+        connectedWallet
+            .post({
+                feeDenoms: ["uusd"],
+                msgs: [
+                    new MsgExecuteContract(
+                        connectedWallet.walletAddress,
+                        tokenAddress,
+                        {
+                            "decrease_allowance": {
+                                "spender": spender,
+                                "amount": allowance,
+                                "expires": {
+                                    "at_height": 1
+                                }
+                            }
+                        },
+                        0
+                    )
+                ]
+            })
+            .then((nextTxResult) => {
+                console.log(nextTxResult);
+                setTxResult(nextTxResult);
+            })
+            .catch((error) => {
+                if (error instanceof UserDenied) {
+                    setTxError('User Denied');
+                } else if (error instanceof CreateTxFailed) {
+                    setTxError('Create Tx Failed: ' + error.message);
+                } else if (error instanceof TxFailed) {
+                    setTxError('Tx Failed: ' + error.message);
+                } else if (error instanceof Timeout) {
+                    setTxError('Timeout');
+                } else if (error instanceof TxUnspecifiedError) {
+                    setTxError('Unspecified Error: ' + error.message);
+                } else {
+                    setTxError('Unknown Error: ' + (error instanceof Error ? error.message : String(error)),);
                 }
-              }
-            },
-            0
-          )
-        ]
-      })
-      .catch((error) => {
-        if (error instanceof UserDenied) {
-          setTxError('User Denied');
-        } else if (error instanceof CreateTxFailed) {
-          setTxError('Create Tx Failed: ' + error.message);
-        } else if (error instanceof TxFailed) {
-          setTxError('Tx Failed: ' + error.message);
-        } else if (error instanceof Timeout) {
-          setTxError('Timeout');
-        } else if (error instanceof TxUnspecifiedError) {
-          setTxError('Unspecified Error: ' + error.message);
-        } else {
-          setTxError('Unknown Error: ' + (error instanceof Error ? error.message : String(error)),
-        );
-      }
-    });
-  }
-  return { revokeAllowance }
+                console.log(error.message);
+            });
+    }
+    return { revokeAllowance }
 }
 
 export default useRevokeAllowance
